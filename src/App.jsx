d1d8148e1c34dbaaccfe32d48c821ed2f3f52eed
JoshLikesToCode <jstone925314@gmail.com>
2025-03-20 20:07:14 -0700

    Adding the ability for vite to run backend and frontend on same port

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,21 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="Some delicious 'za" />
-      <Pizza name="Hawaiian" description="Ham and Pineapple" />
-      <Pizza name="Cheese" description="Cheesy slice of pizza!" />
+      <Pizza
+        name="Pepperoni"
+        description="Some delicious 'za"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="Hawaiian"
+        description="Ham and Pineapple"
+        image="/public/pizzas/hawaiian.webp"
+      />
+      <Pizza
+        name="Cheese"
+        description="Cheesy slice of pizza!"
+        image="/public/pizzas/big_meat.webp"
+      />
     </div>
   );
 };
