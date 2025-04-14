6bf009a1c0ea67c5a3b7fdf6a07e6695174f8be5
JoshLikesToCode <jstone925314@gmail.com>
2025-04-13 19:33:11 -0700

    Adding Order page for ordering pizzas

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="Some delicious 'za"
-        image="/public/pizzas/pepperoni.webp"
-      />
-      <Pizza
-        name="Hawaiian"
-        description="Ham and Pineapple"
-        image="/public/pizzas/hawaiian.webp"
-      />
-      <Pizza
-        name="Cheese"
-        description="Cheesy slice of pizza!"
-        image="/public/pizzas/big_meat.webp"
-      />
+      <Order />
     </div>
   );
 };
